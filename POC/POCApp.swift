ab23fc74187954763878a58d7ebfbab236992375
vino <vino>
2022-06-21 18:09:16 +0530

    Initial Commit

---
+++ POC/POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCApp.swift
+//  POC
+//
+//  Created by vinodh kumar on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
